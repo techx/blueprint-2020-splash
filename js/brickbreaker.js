dd87a5d986d26898c0456934153549ee6331aa1d
Zoë Marschner <zoe.marschner@gmail.com>
2019-12-07 22:57:09 -0500

    brick breaker sort of done

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -7,7 +7,7 @@ const framesPerSecond = 50
 
 const paddleDeltaH = 70 // distance paddle is above bottom of screen
 const paddleAspect = 7.334 // aspect radtio of paddle asset
-const paddleYR = 15 
+const paddleYR = 10 
 paddleY = () => { return gameHeight - paddleDeltaH }
 paddleXR = () => { // computed variable: "x radius" of the ball (i.e. width / 2)
 	return paddleYR * paddleAspect
@@ -36,6 +36,8 @@ let logoSvg = new Image()
 logoSvg.src = ('../assets/svg/blueprintlogo.svg')
 
 // GAME VARIABLES
+let score = 0
+
 let canvas, context
 
 let ballX, ballY
@@ -59,12 +61,14 @@ logoTopX = () => {
 }
 
 let brokenBlocks = []
-let begAnimationTick = 0
-let tickSpeed = 2
+
+let animationTick = 0
+let animationRepeat = 3
+let animationInterval = 17
+let animationOn = 6
 
 /** WINDOW FUNCTIONS **/
-function startBrickGame(logoBounds) {
-	console.log(logoBounds)
+function startBrickGame() {
 	for (var i = 0; i < vertBlocks; i++) {
 		let row = []
 		for (var j = 0; j < horizBlocks; j++) {  row.push(false) }
@@ -73,19 +77,50 @@ function startBrickGame(logoBounds) {
 
 	canvas = document.getElementById('brickerbreaker-canvas')
 	context = canvas.getContext('2d')
-	logoRect = logoBounds
 
 	fixCanvasDim()
 	resetGame()
+
+	animationTick = 0
 
   	setInterval(gameTick, 1000/framesPerSecond)
   	canvas.addEventListener('mousemove', updateMousePos)
 }
 
-window.onresize = function(event) {
+function resetBricks() {
+	brokenBlocks = []
+	const topIncluded = [0,1,3,14,15,19]
+	const bottomIncluded = [10]
+	for (let i = 0; i < vertBlocks; i++) {
+		let row = []
+		for (let j = 0; j < horizBlocks; j++) {  
+			console.log(i, j, (i == 0 && !topIncluded.includes(j)))
+			if ((i == 0 && !topIncluded.includes(j)) || (i == vertBlocks-1 && !bottomIncluded.includes(j))) {
+				console.log("hiiiii")
+				row.push(true) 
+			} else {
+				row.push(false)
+			}
+		}
+		brokenBlocks.push(row)
+	}
+}
 
+function resetGame() {
+	resetBricks()
+
+	ballX = gameWidth/2
+	ballY = ballCollisionH() - ballStartDeltaH
+
+	paddleX = gameWidth/2
+
+	ballVx = 0
+	ballVy = 10
+	console.log(brokenBlocks)
+}
+
+window.onresize = function(event) {
 	fixCanvasDim()
-	//TODO: make sure nothing is out of bounds
 }
 
 function fixCanvasDim() {
@@ -101,9 +136,7 @@ function fixCanvasDim() {
 }
 
 function gameTick() {
-	if (begAnimationTick <= (vertBlocks * horizBlocks) * tickSpeed) {
-		begAnimationTick++
-	}
+	animationTick++
 
 	positionUpdate()
 	drawScene()
@@ -116,7 +149,7 @@ function updateMousePos(event){
  	mouseX = event.clientX - rect.left - root.scrollLeft;
   	mouseY = event.clientY - rect.top - root.scrollTop;
 
-  	//check that in bound
+  	//check that in bounds
   	mouseX = Math.min(Math.max(paddleXR(), mouseX), gameWidth - paddleXR())
   	paddleX = mouseX
 }
@@ -164,66 +197,118 @@ function updatePaddleCollision() {
 }
 
 function updateBoxes() {
-	let blocks = 0
-	const ballCorners = [[1, 1], [-1, -1], [1, -1], [-1, 1]]
-	brokenBlocks.forEach((row, r_index) => {
-		row.forEach((broken, c_index) => {
-			//check whether block is there
-			if (!broken) {
-				blockTopX = logoTopX() + boxSize() * c_index
-				blockTopY = logoRect.y + boxSize() * r_index
+	const boxHeight = boxSize() * vertBlocks
+	const boxWidth = boxSize() * horizBlocks
+	const yInBox = ballY + ballR > logoRect.y && ballY - ballR < logoRect.y + boxHeight
+	const xInBox = ballX + ballR > logoTopX() && ballY - ballR < logoTopX() + boxWidth
 
-				ballCorners.forEach((corner) => {
-					const xCoord = ballX + corner[0] * ballR
-					const yCoord = ballY + corner[1] * ballR
-
-					//check whether block is inside
-					if ((xCoord >= blockTopX && xCoord <= blockTopX + boxSize()) && (yCoord >= blockTopY && yCoord <= blockTopY + boxSize())) {
+	if (yInBox && xInBox) {
+		let flips = [0, 0]
+		brokenBlocks.forEach((row, r_index) => {
+			row.forEach((broken, c_index) => {
+				//check whether block is there
+				if (!broken) {
+					let blockTopX = logoTopX() + boxSize() * c_index
+					let blockTopY = logoRect.y + boxSize() * r_index
+					let rect = rectInCircle(ballX, ballY, ballR, blockTopX, blockTopY, boxSize())
+					if (rect !== null) {
 						brokenBlocks[r_index][c_index] = true
-						blocks++
-					}
-				})
-			}
-		})
-	})
+						score += 1
+						//update which ones to flip
+						if (flips[0] == flips[1]) {
+							flips = rect
+						}
 
-	if (blocks > 0) {
-		ballVy = -ballVy
-		ballVx = - ballVx
+					}
+				}
+			})
+		})
+
+		//console.log(flips)
+
+		if (flips[0] == 1) {
+			ballVx = - ballVx
+		}
+		if (flips[1] == 1) {
+			ballVy = - ballVy
+		}
 	}
 }
 
-function resetGame() {
-	ballX = gameWidth/2
-	ballY = ballCollisionH() - ballStartDeltaH
+/** GEOMETRY **/
+function rectInCircle(cx, cy, rad, x, y, width) {
+	let res = [vertLineIntCircle(cx, cy, width, x, y, rad), vertLineIntCircle(cx, cy, width, x + width, y, rad), horizLineIntCircle(cx, cy, width, x, y, rad), horizLineIntCircle(cx, cy, width, x, y + width, rad)]
+	for (let i = 0; i < res.length; i++) {
+		if (res[i] !== null) {
+			return res[i]
+		}
+	}
 
-	paddleX = gameWidth/2
+	const corners = [[0, 0], [0, 1], [1, 0], [1, 1]]
+	for (let i = 0; i < corners.length; i++) {
+		corner = corners[i]
 
-	ballVx = 0
-	ballVy = 10
+		const xCoord = x + corner[0] * width
+		const yCoord = y + corner[1] * width
+
+		let inCircle = pointInCircle(xCoord, yCoord, cx, cy, rad)
+		if (inCircle !== null) {
+			return inCircle
+		}
+	}
+
+	return pointInGridCell(cx, cy, x, y, width)
+}
+
+function vertLineIntCircle(cx, cy, width, x, y, rad) {
+	if (Math.abs(cx - x) <= rad && (y < cy && y + width >= cy)) {
+		// flip just x
+		return [1, 0]
+	}
+	return null
+}
+
+function horizLineIntCircle(cx, cy, width, x, y, rad) {
+	if (Math.abs(cy - y) <= rad && (x < cx && x + width >= cx)) {
+		// flip just y
+		return [0, 1]
+	}
+	return null
+}
+
+function pointInGridCell(xCoord, yCoord, x, y, width) {
+	let xInside = xCoord >= x && xCoord <= x + width
+	let yInside = yCoord >= y && yCoord <= y + width
+	if (xInside && yInside) {
+		// flip both x and y
+		return [1, 1]
+	}
+	return null
+}
+
+function pointInCircle(x, y, cx, cy, radius) {
+	let dist = Math.sqrt(Math.pow(cx - x, 2) + Math.pow(cy - y, 2))
+	if (dist <= radius) {
+		// flip both x and y
+		return [1, 1]
+	}
+	return null
 }
 
 /** DRAWING **/
 function drawScene() {
 	context.clearRect(0, 0, canvas.width, canvas.height)
 	drawLogo()
-
-	//drawHighlightSquare()
-	if (begAnimationTick < (vertBlocks * horizBlocks) * tickSpeed) {
-		const thisRow = Math.floor(Math.floor(begAnimationTick / tickSpeed) / horizBlocks)
-		const thisCol = Math.floor(begAnimationTick / tickSpeed) % horizBlocks
-		//drawHighlightSquare(thisRow, thisCol)
-	}
-
 	clearSquares()
+
+	context.clearRect(logoTopX(), logoRect.y + boxSize() * (vertBlocks), boxSize() * horizBlocks, 10)
+
+	if (animationTick % animationInterval <= animationOn) {
+		drawGrid()
+	}
 
 	drawPaddle()
 	drawBall()
-}
-
-function drawHighlightSquare(row, col) {
-	context.fillStyle = "#ffffff50"
-	drawSquare(row, col)
 }
 
 function clearSquares() {
@@ -236,10 +321,28 @@ function clearSquares() {
 	})
 }
 
+function drawGrid() {
+	brokenBlocks.forEach((row, r_index) => {
+		row.forEach((broken, c_index) => {
+			if (!broken) {
+				context.lineWidth = 1
+				context.strokeStyle = '#ffffff'
+				strokeSquare(r_index, c_index)
+			}
+		})
+	})
+}
+
 function drawSquare(row, col) {
 	topX = logoTopX() + boxSize() * col
 	topY = logoRect.y + boxSize() * row
 	context.fillRect(topX, topY, boxSize(), boxSize())
+}
+
+function strokeSquare(row, col) {
+	topX = logoTopX() + boxSize() * col
+	topY = logoRect.y + boxSize() * row
+	context.strokeRect(topX, topY, boxSize(), boxSize())
 }
 
 function clearSquare(row, col) {
@@ -258,8 +361,6 @@ function drawBall() {
 }
 
 function drawLogo() {
-	context.fillStyle = "black"
-	context.fillRect(logoTopX(), logoRect.y, boxSize() * horizBlocks, logoRect.height)
 	context.drawImage(logoSvg, logoRect.x, logoRect.y, logoRect.width, logoRect.height)
 }
 
