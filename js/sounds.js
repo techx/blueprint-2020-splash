397c98ebc05ec9c33e710d4e3b37e67e7b9eedb2
Justin Yu <justinyu1618@gmail.com>
2019-12-31 03:37:53 -0500

    changed url for sounds

--- js/sounds.js
+++ js/sounds.js
@@ -9,7 +9,7 @@ sounds = {"select":"select.mp3",
 
 for (let [key, src] of Object.entries(sounds)){
   boink = document.createElement("audio")
-  boink.src = "../assets/sound/" + src 
+  boink.src = "./assets/sound/" + src 
   boink.style.display = "none";
   boink.setAttribute("id",key)
   boink.preload = "auto"
